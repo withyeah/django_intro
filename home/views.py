b92c235ee67247896617c28a4236286fcd9b464d
withyeah <areygim@gmail.com>
2019-02-13 05:19:17 +0000

    05_form data GET POST

--- home/views.py
+++ home/views.py
@@ -16,3 +16,20 @@ def hello(request, name):
 def cube(request, num):
     cubenum = int(num)**3
     return render(request, 'cube.html', {'num':num, 'cubenum':cubenum})
+    
+def ping(request):
+    return render(request, 'ping.html')
+    
+def pong(request):
+    print(request.GET)
+    data = request.GET.get('data')
+    return render(request, 'pong.html', {'data':data})
+    
+def user_new(request):
+    return render(request, 'user_new.html')
+    
+def user_create(request):
+    nickname = request.POST.get('nickname')
+    pwd = request.POST.get('pwd')
+    return render(request, 'user_create.html', {'nickname':nickname, 'pwd':pwd})
+    
