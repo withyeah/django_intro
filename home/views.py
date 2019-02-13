0e2807ed6881d62c8c5dc5019e04fbdac6297482
withyeah <areygim@gmail.com>
2019-02-13 02:27:57 +0000

    03_template variable

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+import random
 
 # Create your views here.
 def index(request):
     return HttpResponse('Welcome to Django !')
+    
+def dinner(request):
+    menus = ['chicken', 'shakeshack', 'inandout']
+    pick = random.choice(menus)
+    return render(request, 'dinner.html', {'menus':menus, 'pick':pick})
